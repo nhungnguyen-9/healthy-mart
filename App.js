a83a7fd3317d4292f5feb76a0de1028d87012603
nhungnguyen-9 <nguyennhung9846@gmail.com>
2024-04-25 08:45:09 +0700

    first-commit

--- App.js
+++ App.js
@@ -2,14 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import 'react-native-gesture-handler'
 import HomeScreen from './screens/HomeScreen';
-// import BotTabNav from './navigation/BotTabNav';
+import BotTabNav from './navigation/BotTabNav';
 
 export default function App() {
   return (
-    // <BotTabNav></BotTabNav>
-    <View>
-      <HomeScreen />
-    </View>
+    <BotTabNav></BotTabNav>
+    // <View>
+    //   <HomeScreen />
+    // </View>
   );
 }
 
