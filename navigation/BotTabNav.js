521f4fd77d0ed28990fdfdf52238bd80dc06a917
nhungnguyen-9 <nguyennhung9846@gmail.com>
2024-05-02 08:10:21 +0700

    fix

--- navigation/BotTabNav.js
+++ navigation/BotTabNav.js
@@ -1,118 +1,59 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { createStackNavigator } from "@react-navigation/stack";
-
-
 import { Ionicons } from "@expo/vector-icons";
 import HomeScreen from "../screens/HomeScreen";
 import LoadingScreen from "../screens/LoadingScreen";
-
 import Login from "../components/Login";
 import Register from "../components/Register";
 import LoginAndRegisterScreen from "../screens/LoginAndRegisScreen";
 import Forgot from "../components/Forgot";
 import SMSForgot from "../components/SMSFogot";
 
-const Tab = createBottomTabNavigator();
-
-export function TabNavigator() {
-    return (
-        <Tab.Navigator>
-            {/* <Tab.Screen name="Profile" component={ProfileStack} /> */}
-            <Tab.Screen name="Home" component={HomeStack} />
-            {/* <Tab.Screen name="MachinesList" component={MachineListStack} /> */}
-        </Tab.Navigator>
-    );
-}
-
 const Stack = createStackNavigator();
 const HomeStack = () => {
-    return (
-        <Stack.Navigator initialRouteName="SMSForgot">
-            <Stack.Screen name="LoginAndRegisterScreen" component={LoginAndRegisterScreen} />
-            <Stack.Screen name="Register" component={Register} />
-            <Stack.Screen name="Login" component={Login} />
-            <Stack.Screen name="Forgot" component={Forgot} />
-            <Stack.Screen name="SMSForgot" component={SMSForgot} />
-        </Stack.Navigator>
-    );
+  return (
+    <Stack.Navigator initialRouteName="SMSForgot">
+      <Stack.Screen name="LoginAndRegisterScreen" component={LoginAndRegisterScreen} />
+      <Stack.Screen name="Register" component={Register} />
+      <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="Forgot" component={Forgot} />
+      <Stack.Screen name="SMSForgot" component={SMSForgot} />
+    </Stack.Navigator>
+  );
 };
 
-// export default function BotTabNav() {
-//     return (
-//         <NavigationContainer>
-//             <Tab.Navigator
-//                 screenOptions={({ route }) => ({
-//                     tabBarShowLabel: false,
-//                     tabBarIcon: ({ focused, color, size }) => {
-//                         let iconName;
+const Tab = createBottomTabNavigator();
 
-//                         if (route.name === "Login") {
-//                             iconName = focused ? "person" : "person-outline";
-//                         }
-//                         // } else if (route.name === 'Detail') {
-//                         //     iconName = focused ? 'cloudy' : 'cloudy-outline';
-//                         // } else if (route.name === 'Location') {
-//                         //     iconName = focused ? 'location' : 'location-outline';
-//                         // } else if (route.name === 'Setting') {
-//                         //     iconName = focused ? 'settings' : 'settings-outline';
-//                         // }
+export default function BotTabNav() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName="Home"
+        screenOptions={({ route }) => ({
+          tabBarShowLabel: false,
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
 
-//                         return <Ionicons name={iconName} size={size} color={color} />;
-//                     },
+            if (route.name === "Loading") {
+              iconName = focused ? "loading" : "home-outline";
+            }
 
-//                     tabBarActiveTintColor: "#70b9be",
-//                     tabBarInactiveTintColor: "gray",
-//                 })}
-//             >
-//                 <Tab.Screen name="Login" component={LoginScreen} />
-//                 {/* <Tab.Screen name="Register" component={RegisterScreen} /> */}
-//                 <Tab.Screen name="Home" component={HomeScreen} />
-//                 {/* <Tab.Screen name="Detail" component={DetailScreen} /> */}
-//             </Tab.Navigator>
-//         </NavigationContainer>
-//     );
-// }
-
-// const Tab = createBottomTabNavigator();
-
-// export default function BotTabNav() {
-//   return (
-//     <NavigationContainer>
-//       <Tab.Navigator
-//         initialRouteName="Home"
-//         screenOptions={({ route }) => ({
-//           tabBarShowLabel: false,
-//           tabBarIcon: ({ focused, color, size }) => {
-//             let iconName;
-
-//             if (route.name === "Loading") {
-//               iconName = focused ? "loading" : "home-outline";
-//             }
-//             // } else if (route.name === 'Detail') {
-//             //     iconName = focused ? 'cloudy' : 'cloudy-outline';
-//             // } else if (route.name === 'Location') {
-//             //     iconName = focused ? 'location' : 'location-outline';
-//             // } else if (route.name === 'Setting') {
-//             //     iconName = focused ? 'settings' : 'settings-outline';
-//             // }
-
-//             return <Ionicons name={iconName} size={size} color={color} />;
-//           },
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
 
 
-//           tabBarActiveTintColor: "#70b9be",
-//           tabBarInactiveTintColor: "gray",
-//         })}
-//       >
-//         <Tab.Screen name="Loading" component={LoadingScreen} />
-//         <Tab.Screen
-//           name="Home"
-//           component={HomeScreen}
-//           options={{ headerShown: false }}
-//         />
-//         {/* <Tab.Screen name="Detail" component={DetailScreen} /> */}
-//       </Tab.Navigator>
-//     </NavigationContainer>
-//   );
-//}
+          tabBarActiveTintColor: "#70b9be",
+          tabBarInactiveTintColor: "gray",
+        })}
+      >
+        <Tab.Screen name="Loading" component={LoadingScreen} />
+        <Tab.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ headerShown: false }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
 
